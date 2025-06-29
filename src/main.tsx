a4d691eed938017eb57708389b837d212dc0aca7
Yug Bhatia <bhatiayug175@gmail.com>
2025-06-29 19:35:25 +0530

    Header fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-
+import './index.css';
+import './css/home.css';
+import './css/ai.css';
+import './css/report.css';
+import './css/Prerna.css';
+import './css/blockchain.css';
 createRoot(document.getElementById("root")!).render(<App/>);
